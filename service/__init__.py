a190a7eba873817a6a3d1992011b089a9d8291ac
surinder-ram <133891780+surinder-ram@users.noreply.github.com>
2025-02-14 14:40:43 +0100

    Update __init__.py (kürzer für flake8)

--- service/__init__.py
+++ service/__init__.py
@@ -12,5 +12,7 @@ app = Flask(__name__)
 log_handlers.init_logging(app, "gunicorn.error")
 
 app.logger.info(70 * "*")
-app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info(
+    "  S E R V I C E   R U N N I N G  ".center(70, "*")
+)
 app.logger.info(70 * "*")
